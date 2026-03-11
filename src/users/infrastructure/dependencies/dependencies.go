553bb5449ca980d9cce1c2dacef34ec3e5aed368
bchanona <bh274266@gmail.com>
2026-03-11 11:07:49 -0600

    fix: correct dependencies.go

--- src/users/infrastructure/dependencies/dependencies.go
+++ src/users/infrastructure/dependencies/dependencies.go
@@ -1,7 +1,7 @@
 package dependencies
 
 import (
-	"log"
+	"fmt"
 
 	"github.com/bchanona/Api_chombi.git/src/helper/config"
 	"github.com/bchanona/Api_chombi.git/src/users/application/usecases"
@@ -10,24 +10,26 @@ import (
 )
 
 var (
-	mySQL *infrastructure.MySQL
+	mySQL infrastructure.MySQL
 )
 
 func Init() {
-    db, err := config.ConnMySQL()
-    if err != nil {
-        log.Fatal("Database connection error:", err)
-    }
+	db, err := config.ConnMySQL()
 
-    mySQL = infrastructure.NewMySQL(db)
+	if err != nil {
+		fmt.Println("Database connection error: ", err)
+		return
+	}
+
+	mySQL = *infrastructure.NewMySQL(db)
 }
 
 func RegisterUserDependencies() *controllers.RegisterUserController {
-	useCase := usecases.NewRegisterUserUseCase(mySQL)
+	useCase := usecases.NewRegisterUserUseCase(&mySQL)
 	return controllers.NewRegisterUserController(useCase)
 }
 
 func LoginUserDependencies() *controllers.LoginController {
-	useCase := usecases.NewLoginUserUseCase(mySQL)
+	useCase := usecases.NewLoginUserUseCase(&mySQL)
 	return controllers.NewLoginController(useCase)
 }
