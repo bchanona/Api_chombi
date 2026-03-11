15a9b846aadb7ec95feaffed01b45d0c91e60eef
bchanona <bh274266@gmail.com>
2026-03-11 11:31:46 -0600

    fix: Correct table names

--- src/users/infrastructure/queries/user_queries.go
+++ src/users/infrastructure/queries/user_queries.go
@@ -1,6 +1,6 @@
 package queries
 
 const (
-	RegisterUserQuery = "INSERT INTO users (id, firstName, lastName, email, password, role_id) values (UUID_TO_BIN(?),?,?,?,?,UUID_TO_BIN(?))"
-	FindByEmailQuery  = "SELECT BIN_TO_UUID(u.id), u.firstName, u.lastName,u.email,u.password,r.name AS role_name FROM users u JOIN roles r ON u.role_id = r.id WHERE u.email = ?"
+	RegisterUserQuery = "INSERT INTO Users (id, firstName, lastName, email, password, role_id) values (UUID_TO_BIN(?),?,?,?,?,UUID_TO_BIN(?))"
+	FindByEmailQuery  = "SELECT BIN_TO_UUID(u.id), u.firstName, u.lastName,u.email,u.password,r.name AS role_name FROM Users u JOIN Roles r ON u.role_id = r.id WHERE u.email = ?"
 )
