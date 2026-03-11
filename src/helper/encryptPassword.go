1aaba07d9f25393e3aacd70d5e5ff15dd77577e4
bchanona <bh274266@gmail.com>
2026-03-11 04:16:52 -0600

    feat: Add password hashing

--- src/helper/encryptPassword.go
+++ src/helper/encryptPassword.go
@@ -17,3 +17,8 @@ func EncryptPassword(password string) (string, error) {
 
 	return string(hash), nil
 }
+
+func ComparePassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
